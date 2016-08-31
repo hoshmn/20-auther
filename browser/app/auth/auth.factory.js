0332bc0ac0f7c17da10cf890db674569909245f1
“Joshua <j.maas.howard@gmail.com>
2016-08-31 11:52:58 -0400

    add user

--- browser/app/auth/auth.factory.js
+++ browser/app/auth/auth.factory.js
@@ -3,12 +3,24 @@ app.factory('AuthFactory',function($http, $log, $state){
 
 	this.login = function(user){
 		return $http.post('/api/auth/login', user)
-		.then(function(x){
-			// console.log(x);
-			$state.go('home')
+		.then(function(loggedInUser){
+			console.log(loggedInUser.data.id);
+			$state.go('user', {id:loggedInUser.data.id});
 		}).catch(function(error){
-			alert('Yo WTF')
-		})
+			alert('Yo WTF');
+		});
+	};
+
+
+	this.signup = function(user){
+		console.log(user);
+		return $http.post('/api/auth/signup', user)
+		.then(function(newUser){
+			console.log(newUser.data.id);
+			$state.go('user', {id:newUser.data.id});
+		}).catch(function(error){
+			alert('Yo who you thank you izzz, dat email already usen');
+		});
 	};
 
 
--- server/api/auth/auth.router.js
+++ server/api/auth/auth.router.js
@@ -18,12 +18,31 @@ router.post('/login', function (req, res, next) {
   })
   .then(function (user) {
     if (!user) {
-      console.log("USER IS NOT VALID!")
+      console.log("USER IS NOT VALID!");
       res.sendStatus(401);
     } else {
-      console.log("USER IS VALID!")
+      console.log("USER IS VALID!");
       req.session.userId = user.id;
-      res.sendStatus(204);
+      console.log('$$$$$$', user.id);
+      res.status(200).json(user);
+    }
+  })
+  .catch(next);
+});
+
+
+router.post('/signup', function (req, res, next) {
+  console.log('you got me', req.body);
+  User.create(req.body)
+  .then(function (user) {
+    if (!user) {
+      console.log("USER IS NOT VALID!");
+      res.sendStatus(401);
+    } else {
+      console.log("USER IS VALID!");
+      req.session.userId = user.id;
+      console.log('$$$$$$', user.id);
+      res.status(200).json(user);
     }
   })
   .catch(next);
